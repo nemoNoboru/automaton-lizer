78c4945293c02129b61a4085d391fc9941b0b92c
Fvieira <felipevieira@protonmail.com>
2016-03-15 21:08:47 +0100

    made linear regresion for tendency

--- core/digester.rb
+++ core/digester.rb
@@ -27,6 +27,7 @@ class Digester
       puts "Average response time tendency for /#{key} : #{buff}"
       buff = average_response_last @blocklog_hash[key]
       puts "Average response time of last requests /#{key} : #{buff}"
+      puts "Grade of certainty #{(cuadratic_error(@blocklog_hash[key]) / mean(@blocklog_hash[key]))}%"
       puts
     end
   end
@@ -39,20 +40,30 @@ class Digester
     buff / blocklogs.size
   end
 
-  def average_response_tendency(blocklogs) # TODO implement median in another function 
+  def mean(blocklogs)
+    sum = 0
+    blocklogs.each do |b|
+      sum += b.timetotal.to_f
+    end
+    sum / blocklogs.size
+  end
+
+  def mean_int(blocklogs)
+    sum = 0
+    counter = 0
+    blocklogs.each do |b|
+      sum += b.to_f
+      counter += 1
+    end
+    sum / counter
+  end
+
+  def average_response_tendency(blocklogs)
     numerator = 0.0
     denumerator = 0.0
     counter = 0.0
-    media_x = 0.0
-    media_y = 0.0
-    blocklogs.each do |blocklog|
-      media_x += blocklog.timetotal.to_f
-      media_y += counter
-      counter += 1
-    end
-    media_x /= blocklogs.size.to_f
-    media_y /= blocklogs.size.to_f
-    counter = 0.0
+    media_x = mean blocklogs
+    media_y = mean_int(0..blocklogs.size)
     blocklogs.each do |blocklog|
       numerator += (blocklog.timetotal.to_f - media_x) * (counter.to_f - media_y)
       denumerator += ((blocklog.timetotal.to_f - media_x)**2)
@@ -63,8 +74,12 @@ class Digester
 
   def cuadratic_error(blocklogs)
     sum = 0
+    average_mean = mean blocklogs
     blocklogs.each do |b|
-      sum += (b.timetotal.to_f- # TODO media
+      sum += ((b.timetotal.to_f - average_mean)**2)
+    end
+    sum
+  end
 
   def average_response_last(blocklogs)
     split = (blocklogs.size / 4) + 1
@@ -72,6 +87,6 @@ class Digester
     (1..split).each do |i|
       buff += blocklogs[-i].timetotal.to_i
     end
-    "#{buff/split}ms"
+    "#{buff / split}ms"
   end
 end
